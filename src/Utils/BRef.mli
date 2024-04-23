9707b288028fbc110d584a4830c011ed358c2e3a
Mikołaj Swoboda <95759171+jauzatopauza@users.noreply.github.com>
2024-04-23 09:29:48 +0200

    Improve documentation of `BRef.mli` (#64)

--- src/Utils/BRef.mli
+++ src/Utils/BRef.mli
@@ -6,14 +6,20 @@
 
 type 'a t
 
+(** Create backtrackable reference. *)
 val create : 'a -> 'a t 
 
+(** Check physical equality. *)
 val equal : 'a t -> 'a t -> bool
 
+(** Get current value. *)
 val get : 'a t -> 'a
 
+(** Set a new current value. If `set` is used inside a bracket,
+  the old value will be saved for potential backtracking 
+  unless it was set within the same bracket. *)
 val set : 'a t -> 'a -> unit
 
 (** Run computation and commit changes on success and
-  * backtrack on failure *)
+  backtrack on failure. *)
 val bracket : (unit -> 'a) -> 'a
