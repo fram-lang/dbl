87edaae02967f42aabf1b552ed50f0eb11ab7caf
Piotr Polesiuk <ppolesiuk@cs.uni.wroc.pl>
2025-11-15 07:47:51 +0100

    More rules of simplification of effect constraints
    This commit introduces two new rules of simplification of effect
    constraints:
    - Setting variant variables with no upper-bounds to the join of their
      lower bounds;
    - Removing redundant constraints.

--- src/EffectInference/ConstrSimplify.ml
+++ src/EffectInference/ConstrSimplify.ml
@@ -86,6 +86,43 @@ let build_bounds st bnd (Constr.CSubeffect(_, eff1, eff2)) =
   let (_, gvars) = T.Effct.view eff1 in
   List.fold_left (build_gvar_bounds st eff2) bnd gvars
 
+(** [set_variant_nb st cs gv] sets [gv] to the join of all its lower-bounds
+    in [cs]. The variable [gv] is assumed to have no positive nor negative
+    occurrence, i.e., it should not appear in [st.pgvs] nor [st.ngvs]. *)
+let set_variant_nb st cs gv =
+  assert (not (T.GVar.Set.mem gv st.pgvs));
+  assert (not (T.GVar.Set.mem gv st.ngvs));
+  let lower_bound =
+    List.fold_left
+      (fun lb (Constr.CSubeffect(_, eff1, eff2)) ->
+        let p = T.Effct.lookup_gvar eff2 gv in
+        if not (IncrSAT.Formula.is_false p) then
+          T.Effct.join lb (T.Effct.guard eff1 p)
+        else
+          lb)
+      T.Effct.pure
+      cs
+  in
+  T.GVar.set gv lower_bound
+
+(** Check if effect [eff1] is trivially a subeffect of [eff2]. *)
+let trivial_subeffect eff1 eff2 =
+  let (tvs, gvs) = T.Effct.view eff1 in
+  List.for_all
+    (fun (x, p1) ->
+      IncrSAT.Formula.implies p1 (T.Effct.lookup_tvar eff2 x))
+    tvs &&
+  List.for_all
+    (fun (gv, p1) ->
+      IncrSAT.Formula.implies p1 (T.Effct.lookup_gvar eff2 gv))
+    gvs
+
+(** Check if constraint [c1] is implied by constraint [c2]. *)
+let subsumes c1 c2 =
+  let (Constr.CSubeffect(_, eff_l1, eff_r1)) = c1 in
+  let (Constr.CSubeffect(_, eff_l2, eff_r2)) = c2 in
+  trivial_subeffect eff_l1 eff_l2 && trivial_subeffect eff_r2 eff_r1
+
 (* ========================================================================= *)
 
 let rec simplify_loop st cs =
@@ -117,10 +154,46 @@ and simplify_negative st cs =
       (T.GVar.Map.bindings bounds)
   in
   match bounds with
-  | [] -> cs
+  | [] -> simplify_variant_nb st cs
   | _ ->
     List.iter (set_negative st) bounds;
     simplify_loop st cs
+
+(** Set variant variables (with no positive nor negative occurrence) with no
+  upper-bound to the join of all lower-bounds. *)
+and simplify_variant_nb st cs =
+  let lhs = lhs_gvars st cs in
+  let rhs = rhs_gvars st cs in
+  let variant_nb =
+    rhs
+    |> Fun.flip T.GVar.Set.diff st.ngvs
+    |> Fun.flip T.GVar.Set.diff st.pgvs
+    |> Fun.flip T.GVar.Set.diff lhs
+  in
+  if T.GVar.Set.is_empty variant_nb then
+    remove_redundant st cs
+  else begin
+    T.GVar.Set.iter (set_variant_nb st cs) variant_nb;
+    simplify_loop st cs
+  end
+
+(** Remove redundant constraints. *)
+and remove_redundant st cs =
+  let modified = ref false in
+  let rec loop acc cs =
+    match cs with
+    | [] ->
+      if !modified then
+        simplify_loop st acc
+      else
+        acc
+    | c :: cs when (List.exists (subsumes c) (acc @ cs)) ->
+      modified := true;
+      loop acc cs
+    | c :: cs ->
+      loop (c :: acc) cs
+  in
+  loop [] cs
 
 let simplify ~scope ~pgvs ~ngvs cs =
   let st = { scope; pgvs; ngvs; irr = [] } in
